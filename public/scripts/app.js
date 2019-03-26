e6d5507f16e92034634437e1e38ea8d2bc174a48
github username <suoarik@gmail.com>
2019-03-26 19:50:17 +0000

    an example of how to render without using class/componenets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,92 @@
 
 console.log("Hello World");
 
+var app = {
+    title: "Indecision App",
+    subTitle: "",
+    options: ["One", "Two"]
+};
+
 var template = React.createElement(
-  "p",
-  null,
-  "changed?"
+    "div",
+    null,
+    React.createElement(
+        "p",
+        null,
+        app.title
+    ),
+    app.subTitle && React.createElement(
+        "p",
+        null,
+        app.subTitle
+    ),
+    app.options.length > 0 ? React.createElement(
+        "p",
+        null,
+        "Here are your options"
+    ) : "No Options",
+    React.createElement(
+        "ol",
+        null,
+        React.createElement("li", null),
+        React.createElement("li", null)
+    )
 );
+
+var count = 0;
+
+var increment = function increment() {
+    count += 1;
+    renderCounterApp();
+    console.log("addOne", count);
+};
+
+var decrement = function decrement() {
+    count -= 1;
+    renderCounterApp();
+    console.log("subtractOne", count);
+};
+
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+    console.log("reset", count);
+};
+
 var appRoot = document.querySelector("#root");
 
-ReactDOM.render(template, appRoot);
+var renderCounterApp = function renderCounterApp() {
+    var templateTwo = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            "Count: ",
+            count
+        ),
+        React.createElement(
+            "button",
+            { onClick: function onClick() {
+                    return increment();
+                } },
+            "+1"
+        ),
+        React.createElement(
+            "button",
+            { onClick: function onClick() {
+                    return decrement();
+                } },
+            "-1"
+        ),
+        React.createElement(
+            "button",
+            { onClick: function onClick() {
+                    return reset();
+                } },
+            "Reset"
+        )
+    );
+    ReactDOM.render(templateTwo, appRoot);
+};
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -1,6 +1,65 @@
 console.log("Hello World")
 
-let template = <p>changed?</p>
+
+const app = {
+    title: "Indecision App",
+    subTitle: "",
+    options: ["One", "Two"]
+}
+
+
+let template = (
+    <div>
+        <p>{app.title}</p>
+        {app.subTitle && <p>{app.subTitle}</p>}
+        {app.options.length > 0 ? 
+            <p>Here are your options</p> 
+            :
+        "No Options"
+        }
+        <ol>
+            <li></li>
+            <li></li>
+        </ol>
+    </div>
+)
+
+
+
+let count = 0;
+
+const increment = () => {
+    count += 1
+    renderCounterApp()
+    console.log("addOne", count)
+}
+
+const decrement = () => {
+    count -= 1
+    renderCounterApp()
+    console.log("subtractOne", count)
+}
+
+const reset = () => {
+    count = 0
+    renderCounterApp()
+    console.log("reset", count)
+}
+
+
+
 let appRoot = document.querySelector("#root")
 
-ReactDOM.render(template, appRoot)
+
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={() => increment()}>+1</button>
+            <button onClick={() => decrement()}>-1</button>
+            <button onClick={() => reset()}>Reset</button>
+        </div>
+    )
+    ReactDOM.render(templateTwo, appRoot)
+}
+renderCounterApp()
