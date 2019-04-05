e1a9e0be273d3f9b99199e0779cb96c9c44b9645
github username <suoarik@gmail.com>
2019-04-05 17:05:42 +0100

    messing with imports and exports

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+import subtract, { square, add } from "./utils.js"
+import { isAdult, canDrink } from "./person.js"
+import sen from "./person"
+
+
 console.log("running app.js")
+
+console.log(square(5), add(500), isAdult(19), isAdult(16), canDrink(22), canDrink(20), subtract(1,2), sen(66), sen(64))
